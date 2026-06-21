99af70ba39dc6b935e5cb14d8ddf2714a74483f1
Dennis Dashkevich <dskecse@gmail.com>
2026-06-21 21:33:18 +0300

    Save extracted texts from PDF per each parser

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import os
+import json
+from pathlib import Path
 from pprint import pprint
 
 from src.pdf_processor import PDFProcessor
 
 
-def extract_text_from_pdf():
+def save_extracted_text(pages: list, parser: str, orig_filename: str) -> None:
+    # Create a directory (do not raise if already present)
+    os.makedirs(os.path.join("data", "extracted_texts"), exist_ok=True)
+
+    orig_filename_without_ext = Path(orig_filename).stem
+    new_filename = f"{parser}_{orig_filename_without_ext}.json"
+    output_path = os.path.join("data", "extracted_texts", new_filename)
+
+    with open(output_path, "w", encoding="utf-8") as file:
+        json.dump(pages, file, indent=2, ensure_ascii=False)
+
+    print(f"Extracted text saved to {output_path}")
+
+
+def extract_text_from_pdf(pdf_path: str = "fy10syb.pdf") -> None:
     """
     Extract text from a PDF using different parsers for comparison later.
     """
@@ -12,29 +29,29 @@ def extract_text_from_pdf():
 
     print("Testing text extraction from PDF\n")
 
-    configurations = [
-        {"parser": "docling"},
-        {"parser": "pdfplumber"},
-        {"parser": "pymupdf"},
-        {"parser": "pypdf"},
+    parsers = [
+        "docling",
+        "pdfplumber",
+        "pymupdf",
+        "pypdf",
     ]
 
-    pdf_path = "fy10syb.pdf"
-
-    for config in configurations:
-        print(f"\nTesting configuration: {config}")
+    for parser in parsers:
+        print(f"\nTesting parser: {parser}")
 
         try:
             pages = processor.process_pdf(
                 pdf_path=pdf_path,
-                parser_name=config["parser"]
+                parser_name=parser
             )
 
             print("First page content:")
             print("-" * 60)
             pprint(pages[0])
         except Exception as e:
-            print(f"Error with configuration {config}: {e}\n")
+            print(f"Error with parser {parser}: {e}\n")
+
+        save_extracted_text(pages, parser=parser, orig_filename=pdf_path)
 
 
 if __name__ == "__main__":
